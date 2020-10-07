1fbfbfa06315f8d3f932af5a795c6f273d8d64c9
yim2000 <yim2000@users.noreply.github.com>
2020-10-07 16:36:34 +0200

    7-10
    7-10

---
+++ Assets/02_Student Folders/YimGoemans_Assets/Scripts/BoulderMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderMove : MonoBehaviour
+{ 
+    Collider triggerZone;
+    //use this for initialization  
+  
+    void Start () {  
+           triggerZone = GetComponent<Collider>();
+    }  
+	
+    //Update is called once per frame  
+    void Update () {  
+        //this is for x axis' movement  
+  
+    }    
+	void OnTriggerEnter(Collider other)
+    {
+        Damageable check = other.GetComponent<Damageable>();
+        if (check == null) return;
+    } 
+}

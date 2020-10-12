a197559a3e8b75a5d9370868759daeb0a0992698
kennethdirker <kennethdirker@hotmail.nl>
2020-10-12 23:44:42 +0200

    Level + wild west stuff
    Probably shouldn't push this to master

---
+++ Assets/02_Student Folders/KennethDirker_Assets/Scripts/JumppackPickup.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class JumppackPickup : MonoBehaviour
+{
+    Pickup m_Pickup;
+
+    void Start()
+    {
+        m_Pickup = GetComponent<Pickup>();
+        DebugUtility.HandleErrorIfNullGetComponent<Pickup, JumppackPickup>(m_Pickup, this, gameObject);
+
+        // Subscribe to pickup action
+        m_Pickup.onPick += OnPicked;
+    }
+
+    void OnPicked(PlayerCharacterController byPlayer)
+    {
+        var jumppack = byPlayer.GetComponent<Jumppack>();
+        if (!jumppack)
+            return;
+
+        if (jumppack.TryUnlock())
+        {
+            m_Pickup.PlayPickupFeedback();
+
+            Destroy(gameObject);
+        }
+    }
+}

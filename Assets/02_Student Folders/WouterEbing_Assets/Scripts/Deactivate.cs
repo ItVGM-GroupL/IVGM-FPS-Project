62772c0dc1d667e4c05fd4c4a84acc2d0ee6bb4a
Wouter E <w.p.j.ebing@umail.leidenuniv.nl>
2020-10-16 14:15:06 +0200

    Changes Wouter

--- Assets/02_Student Folders/WouterEbing_Assets/Scripts/Deactivate.cs
+++ Assets/02_Student Folders/WouterEbing_Assets/Scripts/Deactivate.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Deactivate : MonoBehaviour
 {
+    bool collected = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.SetActive(false);
+        if (!collected) { gameObject.SetActive(false); print("Exit is deactivated");  }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void setCollected()
+    {
+        collected = true;
+        print("Exit is activated");
     }
 }

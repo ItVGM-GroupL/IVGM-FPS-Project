821378911832f9110db3d0f206b578ab277e84c1
Kibakun3 <70638389+Kibakun3@users.noreply.github.com>
2020-10-20 00:08:10 +0200

    Patrick recent changes

---
+++ Assets/02_Student Folders/PatrickvanZon/Scripts/Detonator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detonator : MonoBehaviour
+{
+    public GameObject Bomb;
+    public GameObject BreakableWall;
+    public AudioSource Bombsound;
+    private bool IsExploded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsExploded = false;  
+    }
+
+   
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (!IsExploded)
+        {
+            IsExploded = true;
+            Bomb.SetActive(false);
+            BreakableWall.SetActive(false);
+            Bombsound.Play();
+            
+        }
+
+    }
+}

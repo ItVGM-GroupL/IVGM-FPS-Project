055826f2abb8be17df4465cec22cb6e3afaf97e6
guushertogh <31773433+guushertogh@users.noreply.github.com>
2020-10-20 12:26:58 +0200

    Made part of my level. Fixed some scripting errors. enemycowgirlturret added

---
+++ Assets/FPS/Scripts/ShowRemove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ShowRemove : EditorWindow
+{
+	public string notification;
+
+    [MenuItem("Example/Notification Usage")]
+    static void Initialize()
+    {
+        ShowRemove.GetWindow(typeof(ShowRemove));
+    }
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("Show Notification"))
+        {
+            this.ShowNotification(new GUIContent(notification));
+        }
+
+        if (GUILayout.Button("Remove Notification"))
+        {
+            this.RemoveNotification();
+        }
+    }
+}
+
